244f5b1234e98618cce47708774e842b62555bce
leeson <97571961+leesonaa@users.noreply.github.com>
2024-01-13 04:29:25 +0000

    change

--- entrypoint-h5.sh
+++ entrypoint-h5.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 替换 H5 端配置文件中的数据库连接信息
-sed -i "s/APP_URL=http:\/\/localhost/APP_URL=${H5_APP_URL}/" /var/www/html/.env
-sed -i "s/DB_DATABASE=mydatabase/DB_DATABASE=${H5_DB_DATABASE}/" /var/www/html/.env
-sed -i "s/DB_USERNAME=myuser/DB_USERNAME=${H5_DB_USERNAME}/" /var/www/html/.env
-sed -i "s/DB_PASSWORD=mypassword/DB_PASSWORD=${H5_DB_PASSWORD}/" /var/www/html/.env
+perl -pi -e 's/APP_URL=http:\/\/localhost/APP_URL=$ENV{"H5_APP_URL"}/' /var/www/html/.env
+perl -pi -e 's/DB_DATABASE=mydatabase/DB_DATABASE=$ENV{"H5_DB_DATABASE"}/' /var/www/html/.env
+perl -pi -e 's/DB_USERNAME=myuser/DB_USERNAME=$ENV{"H5_DB_USERNAME"}/' /var/www/html/.env
+perl -pi -e 's/DB_PASSWORD=mypassword/DB_PASSWORD=$ENV{"H5_DB_PASSWORD"}/' /var/www/html/.env
 
 # 启动 Apache
 apache2-foreground
--- entrypoint-pc.sh
+++ entrypoint-pc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 替换 PC 端配置文件中的数据库连接信息
-sed -i "s/\$db['default']['hostname'] = 'localhost';/\$db['default']['hostname'] = '${DB_HOST}';/" /var/www/html/application/config/database.php
-sed -i "s/\$db['default']['username'] = 'root';/\$db['default']['username'] = '${DB_USERNAME}';/" /var/www/html/application/config/database.php
-sed -i "s/\$db['default']['password'] = '';/\$db['default']['password'] = '${DB_PASSWORD}';/" /var/www/html/application/config/database.php
-sed -i "s/\$db['default']['database'] = 'mydatabase';/\$db['default']['database'] = '${DB_DATABASE}';/" /var/www/html/application/config/database.php
+perl -pi -e 's/\$db\['\''default'\''\]\['\''hostname'\''\] = '\''localhost'\'';/\$db['\''default'\'']['\''hostname'\''] = '\''$ENV{"DB_HOST"}'\'';/g' /var/www/html/application/config/database.php
+perl -pi -e 's/\$db\['\''default'\''\]\['\''username'\''\] = '\''root'\'';/\$db['\''default'\'']['\''username'\''] = '\''$ENV{"DB_USERNAME"}'\'';/g' /var/www/html/application/config/database.php
+perl -pi -e 's/\$db\['\''default'\''\]\['\''password'\''\] = '\'''\'';/\$db['\''default'\'']['\''password'\''] = '\''$ENV{"DB_PASSWORD"}'\'';/g' /var/www/html/application/config/database.php
+perl -pi -e 's/\$db\['\''default'\''\]\['\''database'\''\] = '\''mydatabase'\'';/\$db['\''default'\'']['\''database'\''] = '\''$ENV{"DB_DATABASE"}'\'';/g' /var/www/html/application/config/database.php
 
 # 启动 Apache
 apache2-foreground
